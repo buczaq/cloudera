a40f9043a088ccb9f672a9d2fed18d50b1cc1479
Krzysztof Buczak <buczaq@gmail.com>
2020-01-08 22:05:00 +0100

    Update create_file_tree.sh

--- scripts/create_file_tree.sh
+++ scripts/create_file_tree.sh
@@ -1,26 +1,41 @@
-hdfs dfs -chmod 777 /user
+LOG_FILE="create_file_tree.log"
+
+su -c "hdfs dfs -chmod 777 /user" hdfs
+
+su -c "hdfs dfs -mkdir /user/hadoop"
+echo "$(date) Created /user/hadoop directory" >> $LOG_FILE
  
-hdfs dfs -mkdir /user/hadoop
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS" hdfs
+echo "$(date) Created /user/hadoop/HDFS directory" >> $LOG_FILE
+
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS/raw_data" hdfs
+echo "$(date) Created /user/hadoop/HDFS/raw_data directory" >> $LOG_FILE
  
-hdfs dfs -mkdir /user/hadoop/HDFS
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/movie_plots" hdfs
+echo "$(date) Created /user/hadoop/HDFS/raw_data/movie_plots directory" >> $LOG_FILE
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/oscars" hdfs
+echo "$(date) Created /user/hadoop/HDFS/raw_data/oscars directory" >> $LOG_FILE
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/actors_genders" hdfs
+echo "$(date) Created /user/hadoop/HDFS/raw_data/actors_genders directory" >> $LOG_FILE
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/synonyms" hdfs
+echo "$(date) Created /user/hadoop/HDFS/raw_data/synonyms directory" >> $LOG_FILE
  
-hdfs dfs -mkdir /user/hadoop/HDFS/raw_data
+su -c "hdfs dfs -put /home/cloudera/Downloads/database.csv /user/hadoop/HDFS/raw_data/oscars/oscars.csv" hdfs
+echo "$(date) Created /user/hadoop/HDFS/raw_data/oscars/oscars.csv" >> $LOG_FILE
  
-hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/films_plots
-hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/oscars
-hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/actors_genders
-hdfs dfs -mkdir /user/hadoop/HDFS/raw_data/synonyms
+su -c "hdfs dfs -put wiki_movie_plots.csv /user/hadoop/HDFS/raw_data/movie_plots/movie_plots.csv" hdfs
+echo "$(date) Created hadoop/HDFS/raw_data/movie_plots/movie_plots.csv" >> $LOG_FILE > echo
+
+su -c "hdfs dfs -put actors_genders.csv /user/hadoop/HDFS/raw_data/actors_genders/actors_genders.csv" hdfs
+echo "$(date) Created /user/hadoop/HDFS/raw_data/actors_genders/actors_genders.csv" >> $LOG_FILE 
+
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS/MiddleSet" hdfs
+echo "$(date) Created /user/hadoop/HDFS/MiddleSet" >> $LOG_FILE
  
-hdfs dfs -put /home/cloudera/Downloads/database.csv /user/hadoop/HDFS/raw_data/oscars/oscars.csv
+su -c "hdfs dfs -mkdir /user/hadoop/HDFS/ResultSet" hdfs
+echo "$(date) Created /user/hadoop/HDFS/ResultSet" >> $LOG_FILE
  
-hdfs dfs -put wiki_movie_plots.csv /user/hadoop/HDFS/raw_data/films_plots/films_plots.csv
- 
-hdfs dfs -put actors_genders.csv /user/hadoop/HDFS/raw_data/actors_genders/actors_genders.csv
- 
-hdfs dfs -mkdir /user/hadoop/HDFS/MiddleSet
-hdfs dfs -mkdir /user/hadoop/HDFS/ResultSet
- 
-COUNTER=$(hdfs dfs -ls -R /user/hadoop/HDFS | wc -l)
+COUNTER=$(su -c "hdfs dfs -ls -R /user/hadoop/HDFS | wc -l" hdfs)
  
 if [[ $COUNTER -ne 10 ]]
 then
@@ -28,3 +43,4 @@ then
     echo "should be: 10 items"
     echo "is: $COUNTER items"
 fi
+
