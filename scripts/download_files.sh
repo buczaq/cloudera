0614e282e59608d8da00465afd1e798472df6b97
Krzysztof Buczak <buczaq@gmail.com>
2020-01-08 22:05:36 +0100

    Update download_files.sh

--- scripts/download_files.sh
+++ scripts/download_files.sh
@@ -1,4 +1,9 @@
 curl -o oscars.csv https://raw.githubusercontent.com/buczaq/cloudera/master/static_datasets/oscars.csv > /dev/null
+echo "$(date) oscars.csv has been downloaded" >> download_files.log
+
 curl -o wiki_movie_plots.csv https://raw.githubusercontent.com/buczaq/cloudera/master/static_datasets/wiki_movie_plots.csv > /dev/null
+echo "$(date) wiki_movie_plots.csv has been downloaded" >> download_files.log
+
 curl -o actors_genders.csv https://raw.githubusercontent.com/buczaq/cloudera/master/static_datasets/actors_genders.csv > /dev/null
+echo "$(date) actors_genders.csv has been downloaded" >> download_files.log
 
